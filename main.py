9118d9b4c8c968e83a8305df7f7d508a2545ac20
christianniebling <christian.v.niebling@gmail.com>
2022-10-29 10:30:15 -0400

    trimmed the size

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from re import I
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy
 from scipy.misc import electrocardiogram
 
 ecg = electrocardiogram()
-print(ecg)
+ecg_size = ecg.size
+ratio = 1/100
+ecg = ecg[:int(ecg_size * ratio)]
 
 fs = 360
 time = np.arange(ecg.size) / fs
 plt.plot(time, ecg)
 plt.xlabel("time in s")
 plt.ylabel("ECG in mV")
-plt.xlim(9, 10.2)
-plt.ylim(-1, 1.5)
 plt.show()
+
+peaks = scipy.signal.find_peaks(ecg)
+print(peaks)
