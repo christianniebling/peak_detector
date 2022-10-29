50ab4880d89d57e6100bcb1b01fedd97d4b7b52b
christianniebling <christian.v.niebling@gmail.com>
2022-10-29 10:40:18 -0400

    added functions file

--- main.py
+++ main.py
@@ -1,33 +1,12 @@
 from re import I
 import matplotlib.pyplot as plt
 import numpy as np
-import math
 from scipy.misc import electrocardiogram
 from scipy.signal import find_peaks
 import scipy
+from functions import *
 ecg = electrocardiogram()
 print(ecg)
-def rmsValue(arr, n):
-    square = 0
-    mean = 0.0
-    root = 0.0
-    for i in range(0,n):
-        square += (arr[i]**2)
-    mean = (square / (float)(n))
-    root = math.sqrt(mean)
-    return root
-def distancefinder(input):
-    size=len(input)
-    distanceArray = []
-    for x in range(size-1):
-        distanceArray.append(input[x+1]-input[x])
-    return distanceArray
-def NNCounter(input,thresh):
-    counter=0
-    for x in input:
-        if x>thresh:
-            counter += 1
-    return counter
         
 fs = 360
 time = np.arange(ecg.size) / fs
