16b92680c4aecd5a2dd88c6f7e64ae7f87df9ed9
Anthony <anthonygp23@gmail.com>
2022-11-17 21:59:52 -0500

    Added rudimentary BP peak detector

--- edf open.py
+++ edf open.py
@@ -8,6 +8,7 @@ from functions import *
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches 
 import matplotlib.axes
+import matplotlib.lines as lines
 from matplotlib.patches import Ellipse
 from math import pi
 # import cv2 as cv 
@@ -17,14 +18,24 @@ import bioread
 # cv.ellipse(center location (x,y), axes length (x AND Y), start angle, end angle)
 # cv.ellipse((0,0),(256,3), (3,270), 0, 360)
 
+#Open ACQ File
 ECG_source = "Sample_ECG.acq"
 file = bioread.read_file(ECG_source)
 Channel_List=file.channels
+
+
+#Pull BP Data and Peaks
+BP_Data = file.channels[0].raw_data
+BP_Time = file.channels[0].time_index
+BP_fs = len(BP_Data/max(BP_Time))
+BP = BP_Data
+BP_peaks, _ = find_peaks(BP, height = 60, threshold = None, distance = 100, prominence=(40,None), width=None, wlen=None, rel_height=None, plateau_size=None)
+td_BP_peaks = (BP_peaks/BP_fs)
+
+#Pull ECG Data and Peaks and all Variables
 ECG_Data = file.channels[1].raw_data
 Time = file.channels[1].time_index
 ECG_fs = len(ECG_Data)/max(Time)
-BP_Data = file.channels[0].raw_data
-BP_Time = file.channels[0].time_index
 x = ECG_Data
 peaks, _ = find_peaks(x, height = 1.0, threshold = None, distance = 100, prominence=(0.7,None), width=None, wlen=None, rel_height=None, plateau_size=None)
 td_peaks = (peaks / ECG_fs)
@@ -47,7 +58,10 @@ HR = np.round(Num_Beats/(Sampling_Time/60),2)
 NewRRDistancePPlot = np.delete(newRRDistance,-1)
 RRIplusOne = Poincare(newRRDistance)
 
+#Print out all pertinent ECG Information
 print("n = " + str(Num_Beats) + " beats are included for analysis")
+print(len(BP_peaks))
+print(x[BP_peaks])
 print("The total sampling time is " + str(Sampling_Time) + " seconds")
 print("The average heart rate during the sampling time is = " + str(HR) + " BPM")
 print("the mean difference between successive R-R intervals is = " + str(np.round(AvgDiff,3)) + " ms")
@@ -62,21 +76,22 @@ print("SD1 = " + str(np.round(SD1,3)) + " ms")
 print("SD2 = " + str(np.round(SD2,3)) + " ms")
 print("SD1/SD2 = " +str(np.round((SD1/SD2),3)))
 print("The area of the ellipse fitted over the Poincaré Plot (S) is " + str(np.round(S,3)) + " ms^2")
+
+#Start of All ECG Plots 
+
+#Raw ECG
 plt.figure()
 plt.plot(Time,ECG_Data)
 plt.xlabel("time (s)")
 plt.ylabel("ECG (mV)")
 
-# plt.figure()
-# plt.plot(BP_Time, BP_Data)
-# plt.xlabel("time (s)")
-# plt.ylabel("Finger Pressure (mmHg) ")
-
+#ECG with R intervals tagged
 plt.figure()
 plt.title("Raw ECG Signal with R-R Detected")
 plt.plot(x)
 plt.plot(peaks, x[peaks], "x")
 
+#RRI 
 plt.figure()
 plt.plot(td_peaks_adjusted, newRRDistance)
 plt.title("RRI")
@@ -84,12 +99,11 @@ plt.xlabel("time (s)")
 plt.ylabel("RRI (ms)")
 plt.ylabel("ECG (mV)")
 
+#Poincare Plot (RRI, RRI + 1)
 EllipseCenterX = np.average(NewRRDistancePPlot)
 EllipseCenterY = np.average(RRIplusOne)
 Center_coords=EllipseCenterX,EllipseCenterY
-# Ellipse_top=SD1*2
-# Ellipse_maxwidth=SD2*2
-plt.figure()
+fig = plt.figure()
 ax=plt.axes()
 z = np.polyfit(NewRRDistancePPlot, RRIplusOne, 1)
 p = np.poly1d(z)
@@ -98,15 +112,26 @@ theta=np.degrees(np.arctan(slope))
 plt.title("Poincaré Plot")
 plt.scatter(NewRRDistancePPlot, RRIplusOne)
 #create ellipse parameters, xy coordinates for center, width of ellipse, height of ellipse, angle of ellipse, colors of outline and inside
-e=Ellipse((Center_coords),(SD2*2),(SD1*2),theta, edgecolor='black',facecolor='none')
+e=Ellipse((Center_coords),SD2*2,SD1*2,theta, edgecolor='black',facecolor='none')
 matplotlib.axes.Axes.add_patch(ax,e)
 plt.plot(NewRRDistancePPlot, p(NewRRDistancePPlot), color="red")
-# plt.plot([Center_coords],[Ellipse_top])
-# plt.plot([Center_coords,[Ellipse_maxwidth]])
 plt.ylabel("RRI + 1 (ms)")
 plt.xlabel("RRI (ms)")
+
+#Raw BP Data 
+plt.figure()
+plt.plot(BP_Time, BP_Data)
+plt.xlabel("time (s)")
+plt.ylabel("Finger Pressure (mmHg) ")
+
+#Systolic Tagged
+plt.figure()
+plt.plot(BP)
+plt.plot(BP_peaks, BP[BP_peaks], "x")
+
+
+
 plt.show()
-# 
 
 # data = mne.io.read_raw_edf(file)
 # raw_data = data.get_data()
@@ -130,4 +155,3 @@ plt.show()
 
 # t = np.linspace(0, 2*pi, 100)
 # plt.plot( u+a*np.cos(t) , v+b*np.sin(t) )
-# plt.show()
