d4c257d2b390ac4653d7b228f3513ae82f76123e
christianniebling <christian.v.niebling@gmail.com>
2023-01-19 20:31:11 -0500

    Christian made changes

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+from enum import Enum
 
 #def generate_sin_wave(freq, fs, duration):
  #   x=np.linspace(0, duration, fs * duration, endpoint=False)
@@ -122,6 +123,59 @@ def DownCount(input1,input2,RRthresh, BPthresh):
                         count += 1
                 return count 
 
+def count(RR_input, BP_input, RR_thresh, BP_thresh):
+
+    class direction(Enum):
+        nil = 1
+        up = 2
+        down = 3
+
+    up_count = 0
+    down_count = 0
+    qualifying_count_num = 3
+    temp_count = 0
+    d1 = direction.nil
+    size = len(RR_input)
+
+    if len(RR_input) != len(BP_input):
+        print('[count]: lengths are not equal')
+
+    for i in range(1, size):
+        if RR_input[i] <= (RR_input[i-1] - RR_thresh) and BP_input[i] >= (BP_input[i-1] + BP_thresh): # up
+            if d1 == direction.down:
+                if temp_count >= qualifying_count_num:
+                    down_count += 1
+                temp_count = 0
+
+            temp_count += 1
+            d1 = direction.up
+        elif RR_input[i] >= (RR_input[i-1] + RR_thresh) and BP_input[i] <= (BP_input[i-1] - BP_thresh): # down
+            if d1 == direction.up:
+                if temp_count >= qualifying_count_num:
+                    up_count += 1
+                temp_count = 0
+
+            temp_count += 1
+            d1 = direction.down
+        else:
+            if temp_count >= qualifying_count_num:
+                if d1 == direction.up:
+                    up_count += 1
+                elif d1 == direction.down:
+                    down_count += 1
+                    
+            temp_count = 0
+            d1 = direction.nil
+
+        # if i == size-1:
+        #     if temp_count >= qualifying_count_num:
+        #         if d1 == direction.up:
+        #             up_count += 1
+        #         elif d1 == direction.down:
+        #             down_count += 1
+
+    return up_count, down_count
+ 
 
 
 
--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import bioread
 
 
 #Open ACQ File
-ECG_source = "c:/Users/Anthony/Desktop/OLD KINDERHOOK/peak_detector/REST.acq"
+ECG_source = "REST.acq"
 file = bioread.read_file(ECG_source)
 Channel_List=file.channels
 
@@ -180,8 +180,8 @@ TrimmedBP_time = TimeTrimmer(BP_Time, 60)
 # TestDownDownEvents = str(DownCount(Fake_RR_Array, Fake_BP_Array,4,1))
 # print ("The number of Up Up Events in this sample is " + TestUpUpEvents)
 # print("The number of Down Down Events in this sample is " + TestDownDownEvents)
-UpUpEvents = UpCount(newRRDistance,Systolic_Array,4,1)
-DownDownEvents = DownCount(newRRDistance,Systolic_Array,4,1)
+UpUpEvents = count(newRRDistance,Systolic_Array,4,1)
+DownDownEvents = count(newRRDistance,Systolic_Array,4,1)
 TotalEvents = UpUpEvents + DownDownEvents
 print ("The number of Up Up Events in this sample is " + str(UpUpEvents))
 print("The number of Down Down Events in this sample is " + str(DownDownEvents))
