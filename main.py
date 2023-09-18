af652a340a042d7427feae315684cea62d92a8c8
Anthony <anthonygp23@gmail.com>
2023-09-18 19:48:57 -0400

    yes

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches 
 import matplotlib.axes
 import matplotlib.lines as lines
-import pywt
+# import pywt
 from matplotlib.patches import Ellipse
 from math import pi
 import bioread
@@ -19,23 +19,23 @@ import bioread
 
 
 #Open ACQ File
-ECG_source = "data/REST.acq"
+ECG_source = "data/NH07 POST ECG.acq"
 file = bioread.read_file(ECG_source)
 Channel_List=file.channels
 
 
 
 #Pull BP Data 
-BP_Data = file.channels[0].raw_data
-BP_Time = file.channels[0].time_index
-BP_fs = len(BP_Data)/max(BP_Time)
-BP = BP_Data
-BP_peaks, _ = find_peaks(BP, height = 50, threshold = None, distance = 100, prominence=(40,None), width=None, wlen=None, rel_height=None, plateau_size=None)
-td_BP_peaks = (BP_peaks/BP_fs)
+# BP_Data = file.channels[0].raw_data
+# BP_Time = file.channels[0].time_index
+# BP_fs = len(BP_Data)/max(BP_Time)
+# BP = BP_Data
+# BP_peaks, _ = find_peaks(BP, height = 50, threshold = None, distance = 100, prominence=(40,None), width=None, wlen=None, rel_height=None, plateau_size=None)
+# td_BP_peaks = (BP_peaks/BP_fs)
 
 #Pull ECG Data  and all Variables
-ECG_Data = file.channels[1].raw_data
-Time = file.channels[1].time_index
+ECG_Data = file.channels[0].raw_data
+Time = file.channels[0].time_index
 ECG_fs = len(ECG_Data)/max(Time)
 x = ECG_Data
 
@@ -46,22 +46,22 @@ x = ECG_Data
 # TrimmedBP_time = TimeTrimmer(BP_Time, 60)
 
 #Tag R Intervals and create Array of RR Interval Distances
-peaks, _ = find_peaks(x, height = 0.9, threshold = None, distance = 100, prominence=(0.7,None), width=None, wlen=None, rel_height=None, plateau_size=None)
+peaks, _ = find_peaks(x, height = 0.8, threshold = None, distance = 100, prominence=(0.7,None), width=None, wlen=None, rel_height=None, plateau_size=None)
 td_peaks = (peaks / ECG_fs)
 RRDistance = distancefinder(td_peaks)
 #convert to ms
 RRDistance_ms = [element * 1000 for element in RRDistance]
 
-#Tag Systolic BP Peaks Untrimmed and trimmed
-BP_peaks, _ = find_peaks(BP, height = 50, threshold = None, distance = 100, prominence=(40,None), width=None, wlen=None, rel_height=None, plateau_size=None)
-td_BP_peaks = (BP_peaks/BP_fs)
-# Trimmed_BP_peaks, _ = find_peaks(TrimmedBP, height = 50, threshold = None, distance = 100, prominence=(40,None), width=None, wlen=None, rel_height=None, plateau_size=None)
-# Trimmed_td_BP_peaks = (Trimmed_BP_peaks/BP_fs)
-# Trimmed_Systolic_Array = TrimmedBP[Trimmed_BP_peaks]
-#Obtain pulse interval (time difference between BP peaks)
-PulseIntervalDistance = distancefinder(td_BP_peaks)
-#Convert to ms
-PI_ms = [element * 1000 for element in PulseIntervalDistance]
+# #Tag Systolic BP Peaks Untrimmed and trimmed
+# BP_peaks, _ = find_peaks(BP, height = 50, threshold = None, distance = 100, prominence=(40,None), width=None, wlen=None, rel_height=None, plateau_size=None)
+# td_BP_peaks = (BP_peaks/BP_fs)
+# # Trimmed_BP_peaks, _ = find_peaks(TrimmedBP, height = 50, threshold = None, distance = 100, prominence=(40,None), width=None, wlen=None, rel_height=None, plateau_size=None)
+# # Trimmed_td_BP_peaks = (Trimmed_BP_peaks/BP_fs)
+# # Trimmed_Systolic_Array = TrimmedBP[Trimmed_BP_peaks]
+# #Obtain pulse interval (time difference between BP peaks)
+# PulseIntervalDistance = distancefinder(td_BP_peaks)
+# #Convert to ms
+# PI_ms = [element * 1000 for element in PulseIntervalDistance]
 
 #Time domain HRV Variables
 Successive_time_diff=SuccessiveDiff(RRDistance_ms)
@@ -103,13 +103,13 @@ print("SD1/SD2 = " + str(np.round((SD1/SD2),3)))
 print("The area of the ellipse fitted over the Poincaré Plot (S) is " + str(np.round(S,3)) + " ms^2")
 
 #Blood Pressure Information
-Systolic_Array = BP[BP_peaks]
-Avg_BP = np.round((np.average(Systolic_Array)),3)
-SD_BP = np.round((np.std(Systolic_Array)),3)
+# Systolic_Array = BP[BP_peaks]
+# Avg_BP = np.round((np.average(Systolic_Array)),3)
+# SD_BP = np.round((np.std(Systolic_Array)),3)
 
 #Print Blood Pressure Information
-print("The average systolic blood pressure during the sampling time is " + str(Avg_BP) + " + - " + str(SD_BP) + " mmHg")
-print(str(len(Systolic_Array)) + " pressure waves are included in the analysis")
+# print("The average systolic blood pressure during the sampling time is " + str(Avg_BP) + " + - " + str(SD_BP) + " mmHg")
+# print(str(len(Systolic_Array)) + " pressure waves are included in the analysis")
 
 #Start of All ECG Plots 
 #Raw ECG
@@ -161,95 +161,119 @@ plt.text(950, 700, 'SD2 = ' + str(np.round((SD2),1)) + "ms", fontsize=10)
 
 #Start of BP Plots 
 # #Raw BP Data 
-plt.figure()
-plt.plot(BP_Time, BP_Data)
-plt.xlabel("time (s)")
-plt.ylabel("Finger Pressure (mmHg) ")
+# plt.figure()
+# plt.plot(BP_Time, BP_Data)
+# plt.xlabel("time (s)")
+# plt.ylabel("Finger Pressure (mmHg) ")
 
-# #Systolic Tagged
-plt.figure()
-plt.plot(BP)
-plt.plot(BP_peaks, BP[BP_peaks], "x")
-plt.ylabel("Blood Pressure (mmHg)")
-plt.title("Raw BP with Systolic Detected")
+# # #Systolic Tagged
+# plt.figure()
+# plt.plot(BP)
+# plt.plot(BP_peaks, BP[BP_peaks], "x")
+# plt.ylabel("Blood Pressure (mmHg)")
+# plt.title("Raw BP with Systolic Detected")
 
 #Any type of preprocessing for FFT: Resampling, windowing, filtering, etc.
 
 #Tachogram Resampling
 sampling_rate = 250
-resampled_tachogram, resampled_sampling_rate = resample_tachogram(RRDistance_ms, ECG_fs, 250)
+resampled_tachogram, resampled_sampling_rate = resample_tachogram(RRDistance_ms, ECG_fs, 100000)
 original_time = np.arange(0, len(RRDistance_ms)/ECG_fs, 1/ECG_fs)
 resampled_time = np.arange(0, len(resampled_tachogram)/resampled_sampling_rate, 1/resampled_sampling_rate)
 
 #Windowing
-window = np.hanning(len(RRDistance_ms))
-windowed_RRI = RRDistance_ms * window 
+# window = np.hanning(len(RRDistance_ms))
+# windowed_RRI = RRDistance_ms * window 
 
 #Plot original + Windowed RRI 
-plt.figure(figsize=(10, 6))
-plt.plot(RRDistance_ms, label='Original Signal')
-plt.plot(windowed_RRI, label='Windowed Signal')
-plt.xlabel('Sample')
-plt.ylabel('Amplitude')
-plt.title('ECG Signal with Hann Windowing')
-plt.legend()
-# plt.show()
+# plt.figure(figsize=(10, 6))
+# plt.plot(RRDistance_ms, label='Original Signal')
+# plt.plot(windowed_RRI, label='Windowed Signal')
+# plt.xlabel('Sample')
+# plt.ylabel('Amplitude')
+# plt.title('ECG Signal with Hann Windowing')
+# plt.legend()
+# # plt.show()
 
 #Low Pass Filtering (eliminate noise from high frequency bands)
-cutoff_freq = 0.5
-filter_order = 4 
-nyquist_freq = 0.5 * len(windowed_RRI)
-cutoff = cutoff_freq/nyquist_freq
-#Apply filter
-b, a = signal.butter(filter_order, cutoff, btype = 'low')
-filtered_windowed_RRI = signal.lfilter(b,a, windowed_RRI)
+# cutoff_freq = 0.5
+# filter_order = 4 
+# nyquist_freq = 0.5 * len(windowed_RRI)
+# cutoff = cutoff_freq/nyquist_freq
+# #Apply filter
+# b, a = signal.butter(filter_order, cutoff, btype = 'low')
+# filtered_windowed_RRI = signal.lfilter(b,a, windowed_RRI)
 
-#Plot original + filtered RRI 
-plt.figure(figsize=(10, 6))
-plt.plot(windowed_RRI, label='Original Signal')
-plt.plot(filtered_windowed_RRI, label='Filtered Signal')
-plt.xlabel('Sample')
-plt.ylabel('Amplitude')
-plt.title('ECG Signal with Low-Pass Filtering')
-plt.legend()
-
-
-
-#FFT 
-fft_result = np.fft.fft(windowed_RRI)
-frequencies = np.fft.fftfreq(len(windowed_RRI))
-
-#Calculate Power Spectral Density
-psd = np.abs(fft_result)**2
-
-#Plot FFT Result
-plt.figure()
-plt.plot(frequencies[:len(filtered_windowed_RRI)//2], np.abs(fft_result[:len(filtered_windowed_RRI)//2]))
-plt.title('FFT Result')
-plt.xlabel('Frequency')
-plt.ylabel('Magnitude')
+# #Plot original + filtered RRI 
+# plt.figure(figsize=(10, 6))
+# plt.plot(windowed_RRI, label='Original Signal')
+# plt.plot(filtered_windowed_RRI, label='Filtered Signal')
+# plt.xlabel('Sample')
+# plt.ylabel('Amplitude')
+# plt.title('ECG Signal with Low-Pass Filtering')
+# plt.legend()
 
 
-#Plot FFT with PSD Instead of Magnitude
-plt.figure()
-plt.plot(frequencies[:len(filtered_windowed_RRI)//2], psd[:len(filtered_windowed_RRI)//2])
-plt.title('Power Spectral Density (PSD)')
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('PSD')
+
+# #FFT 
+# fft_result = np.fft.fft(RRDistance_ms)
+# frequencies = np.fft.fftfreq(RRDistance_ms)
+
+# #Calculate Power Spectral Density
+# psd = np.abs(fft_result)**2
+
+# #Plot FFT Result
+# plt.figure()
+# plt.plot(frequencies[:len(RRDistance_ms)//2], np.abs(fft_result[:len(RRDistance_ms)//2]))
+# plt.title('FFT Result')
+# plt.xlabel('Frequency')
+# plt.ylabel('Magnitude')
+
+
+# #Plot FFT with PSD Instead of Magnitude
+# plt.figure()
+# plt.plot(frequencies[:len(RRDistance_ms)//2], psd[:len(RRDistance_ms)//2])
+# plt.title('Power Spectral Density (PSD)')
+# plt.xlabel('Frequency (Hz)')
+# plt.ylabel('PSD')
+
+def plot_power_spectral_density(rri, sampling_rate):
+    # Perform Fast Fourier Transform (FFT) on the R-R intervals
+    freq, psd = signal.welch(rri, fs=sampling_rate, nperseg=len(rri))
+    
+    # Keep frequencies in the range of 0.01 Hz to 0.4 Hz (typical HRV frequency range)
+    mask = (freq >= 0.01) & (freq <= 0.4)
+    freq = freq[mask]
+    psd = psd[mask]
+    
+    # Plot the Power Spectral Density
+    plt.figure(figsize=(10, 6))
+    plt.semilogy(freq, psd)
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Power/Frequency (log scale)')
+    plt.title('Power Spectral Density of R-R intervals (HRV Frequency Range)')
+    plt.grid(True)
+    # plt.show()
+
+plot_power_spectral_density(RRDistance_ms, ECG_fs)
+
+
+
+    # Plot Power Spectral Density
 
 #Try Discrete Wavelet Transform Instead
-wavelet = 'db4'
-level = 5
-coeffs = pywt.wavedec(filtered_windowed_RRI, wavelet, level = level)
+# wavelet = 'db4'
+# level = 5
+# coeffs = pywt.wavedec(filtered_windowed_RRI, wavelet, level = level)
 
 #Plot DWT 
-plt.figure()
-for i in range (level + 1): 
-    plt.subplot(level+1, 1, i+1)
-    plt.plot(coeffs[i])
-    plt.title(f'DWT Coefficients - Level {i}')
-    plt.xlabel('Index')
-    plt.ylabel('Coefficient')
+# plt.figure()
+# for i in range (level + 1): 
+#     plt.subplot(level+1, 1, i+1)
+#     plt.plot(coeffs[i])
+#     plt.title(f'DWT Coefficients - Level {i}')
+#     plt.xlabel('Index')
+#     plt.ylabel('Coefficient')
 # plt.show()
 
 #plot resampled tachogram
@@ -273,20 +297,22 @@ plt.legend()
 # plt.ylabel("Finger Pressure (mmHg) ")
 
 #Count BP Ramps
-BpUpRamps,BpDownRamps = bpCount(Systolic_Array,1)
-TotalRamps = BpUpRamps + BpDownRamps
-print(str(BpUpRamps) + " SBP Up Ramps were observed during the Recording Period")
-print(str(BpDownRamps) + " SBP Down Ramps were observed during the Recording Period")
-print(str(TotalRamps) + " Total SBP Ramps were observed during the Recording Period")
+# BpUpRamps,BpDownRamps = bpCount(Systolic_Array,1)
+# TotalRamps = BpUpRamps + BpDownRamps
+# print(str(BpUpRamps) + " SBP Up Ramps were observed during the Recording Period")
+# print(str(BpDownRamps) + " SBP Down Ramps were observed during the Recording Period")
+# print(str(TotalRamps) + " Total SBP Ramps were observed during the Recording Period")
 
 #Count PI + BP Ramps
-UpEvents, DownEvents = count(PI_ms, np.delete(Systolic_Array,-1), 4, 1)
+# UpEvents, DownEvents = count(PI_ms, np.delete(Systolic_Array,-1), 4, 1)
 # print(UpEvents) #+ " PI/SBP up-up events were observed during the Recording Period"
 # print(DownEvents) #+ " PI/SBP down-down events were observed during the Recording Period"
 
 #Approximate Entropy
-print(ApEn(RRDistance_ms,2,0.2 * np.std(RRDistance_ms)))
-print(SampEn(RRDistance_ms,2,0.2 * np.std(RRDistance_ms)))
+print('ApEn is ' + str(ApEn(RRDistance_ms,2,0.2 * np.std(RRDistance_ms))))
+print('SampEn is ' + str(SampEn(RRDistance_ms,2,0.2 * np.std(RRDistance_ms))))
+RRDistance_ms.sort()
+print(RRDistance_ms)
 
 
 # print(ApEn(sin_y, 2, np.std(sin_y) * 0.2))
